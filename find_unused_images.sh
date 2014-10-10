4ca39ed6f3412a4ac7d136e9792b3ff04bf40864
Ladislav Gubik <lacogubik@gmail.com>
2014-10-10 19:43:09 +0100

    Adding jpeg to image search

--- find_unused_images.sh
+++ find_unused_images.sh
@@ -5,6 +5,7 @@ MYPATH=$1
 find "$MYPATH" -name *.jpg -exec basename {} \; > /tmp/patterns
 find "$MYPATH" -name *.png -exec basename {} \; >> /tmp/patterns
 find "$MYPATH" -name *.gif -exec basename {} \; >> /tmp/patterns
+find "$MYPATH" -name *.jpeg -exec basename {} \; >> /tmp/patterns
 
 for p in $(cat /tmp/patterns); do
     grep -R $p "$MYPATH" > /dev/null || echo $p;
